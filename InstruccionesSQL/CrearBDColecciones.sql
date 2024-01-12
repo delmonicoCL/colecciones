be6f1081c586f5fdf4f3b4b8103ca6fe3d42a69d
Marcelo <taiker17@gmail.com>
2024-01-12 19:13:13 +0100

    bd

--- InstruccionesSQL/CrearBDColecciones.sql
+++ InstruccionesSQL/CrearBDColecciones.sql
@@ -2,21 +2,21 @@ CREATE DATABASE colecciones;
 USE colecciones;
 
 CREATE TABLE Artista (
-    ID_Artista INT PRIMARY KEY,
+    ID_Artista INT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(255)
 );
 
 CREATE TABLE Albums (
-    ID_Albums INT PRIMARY KEY,
+    ID_Albums INT PRIMARY KEY AUTO_INCREMENT,
     ID_Artista INT,
     Nombre VARCHAR(255),
     Descripcion TEXT,
-    Imagen BLOB,
+    Imagen VARCHAR(255),
     FOREIGN KEY (ID_Artista) REFERENCES Artista(ID_Artista)
 );
 
 CREATE TABLE Canciones (
-    ID_Canciones INT PRIMARY KEY,
+    ID_Canciones INT PRIMARY KEY AUTO_INCREMENT,
     ID_Albums INT,
     Nombre VARCHAR(255),
     Duracion TIME,
@@ -24,7 +24,7 @@ CREATE TABLE Canciones (
 );
 
 CREATE TABLE Estilos (
-    ID_Estilos INT PRIMARY KEY,
+    ID_Estilos INT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(255),
     Origen VARCHAR(255)
 );
